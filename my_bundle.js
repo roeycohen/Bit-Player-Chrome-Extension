433af1c3999103cd3218c212a22c8de0596d65ef
Roey <roeycohen@gmail.com>
2016-02-25 23:11:14 +0200

    _

--- my_bundle.js
+++ my_bundle.js
@@ -4,29 +4,52 @@
 //},
 //then run: npm run my_bundle
 
-process.hrtime = require('browser-process-hrtime');
-window.localStorage = chrome.storage.local;
-fs = require('fs');
-fs.existsSync = function(path)
+if (typeof window !== 'undefined')
 {
-	console.log('existsSync', path);
-	return true;
-};
+	process.hrtime = require('browser-process-hrtime');
+	window.localStorage = chrome.storage.local;
+	fs = require('fs');
+	fs.existsSync = function(path)
+	{
+		console.log('existsSync', path);
+		return true;
+	};
+}
+
 var torrents = require('torrent-stream');
+var mem = require('memory-chunk-store');
 
-//var mem = require('torrent-memory-storage');
-
-var test_torrent = 'magnet:?xt=urn:btih:090c797d6c3bdcdae733527d9a275586ca5b55ae&dn=The+Big+Bang+Theory+S09E07+HDTV+x264+REPACK-LOL&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80&tr=udp%3A%2F%2Fopen.demonii.com%3A1337&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Fexodus.desync.com%3A6969';
+var test_torrent = 'magnet:?xt=urn:btih:b662dbf7f84740b9fa1a332ce42c3f2859727134&dn=Marvels.Jessica.Jones.S01E05.WEBRip.XviD-FUM%5Bettv%5D&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80&tr=udp%3A%2F%2Fopen.demonii.com%3A1337&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Fexodus.desync.com%3A6969';
 
 var engine = torrents(test_torrent, {
-	//storage: mem
+	storage: mem
 });
-console.log('engine', engine);
+
 engine.on('ready', function ()
 {
-	console.log('engine ready', engine);
 	engine.files.forEach(function (file)
 	{
-		console.log('filename:', file.name);
+		if (file.name === 'Torrent-Downloaded-from-ExtraTorrent.cc.txt')
+		{
+			console.log('filename:', file.name);
+			var stream = file.createReadStream();
+
+			stream.on("data", function (chunk)
+			{
+				console.log('got %d bytes of data', chunk.length, chunk);
+			});
+			stream.on("close", function ()
+			{
+				console.log("close");
+				//stream.destroy();
+			});
+			stream.on("end", function ()
+			{
+				console.log("end");
+				stream.destroy();
+			});
+
+			//stream.pipe(process.stdout);
+		}
 	});
 });
